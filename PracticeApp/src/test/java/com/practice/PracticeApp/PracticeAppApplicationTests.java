4310af840f600446eac44461e0e0a75db715c428
Rishi Airan <20bcs042@ietdavv.edu.in>
2024-10-03 23:51:36 +0530

    initial commit

---
+++ PracticeApp/src/test/java/com/practice/PracticeApp/PracticeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.PracticeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
